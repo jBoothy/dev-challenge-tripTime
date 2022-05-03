5564a7bc4d12039391f53ae0e2ded97ef01d66b3
John Booth <jhunter.booth@gmail.com>
2022-05-03 10:14:25 -0600

    finished challenge

--- tripTime.js
+++ tripTime.js
@@ -1,10 +1,36 @@
 const tripTime = (arr) => {
+
+
     let result = {
         avgSpeedLimits: [], 
         segmentTimes: [], 
         totalTime: null
     }
+    let totalTime = arr.reduce((acc, curr) => {
 
+    let segmentDistance = curr.speedLimits.reduce((speedAcc, obj) => {
+        return speedAcc += obj.distance
+    }, 0)
+
+    let {traffic} = curr
+    let avgSpeedLimit = 0
+    let segmentTime = 0
+
+    curr.speedLimits.forEach(obj => {
+        avgSpeedLimit += Math.round(obj.speedLimit * (obj.distance / segmentDistance))
+        let objTime = Math.round((obj.distance * (obj.speedLimit / 60)) /60) + obj.traffic
+        segmentTime += objTime
+    })
+        
+    segmentTime = Math.round(segmentTime)
+    result.segmentTimes.push(segmentTime)
+    acc += segmentTime
+
+    result.avgSpeedLimits.push(avgSpeedLimit)
+        return acc
+    }, 0)
+
+    result.totalTime = totalTime
 
     return result
 }
@@ -102,4 +128,4 @@ const stops = [
 ]
 
 
-// tripTime(stops)
+console.log(tripTime(stops))
